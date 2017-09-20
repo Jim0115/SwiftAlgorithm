2ba3b6a71496823a68f9b405a88a9e5e2f24912a
Wangshijie <bjwangshijie@corp.netease.com>
2017-09-20 20:27:32 +0800

    Update Boyer-Moore's comments

--- SwiftAlgorithm/SwiftAlgorithm/Boyer-Moore.swift
+++ SwiftAlgorithm/SwiftAlgorithm/Boyer-Moore.swift
@@ -16,6 +16,7 @@ extension String {
         guard patternLength > 0, patternLength <= characters.count else { return nil }
         
         // 进行匹配时，使用skipTable决定跳过的长度
+        // 如果在pattern存在相同字符，则以小的为准，因为需要优先对其距离结尾更近的字符
         var skipTable = [Character : Int]()
         pattern.enumerated().forEach {
             skipTable[$1] = patternLength - $0 - 1
@@ -31,7 +32,7 @@ extension String {
         var i = index(startIndex, offsetBy: patternLength - 1)
         
         
-        // 从两个字符串的末尾开始向前匹配，如果知道pattern的头部都相同，则返回self对应的index，即为pattern的index
+        // 从两个字符串的末尾开始向前匹配，如果直到pattern的头部都相同，则返回self对应的index，即为pattern的index
         // 否则返回nil
         func backwards() -> Index? {
             var q = p
@@ -55,10 +56,11 @@ extension String {
                 if let k = backwards() { return k }
                 
                 // 匹配不成功，将匹配开始位置向后移动一位
+                // 移动一位是因为如果使用skipTable则会后移0位
                 i = index(after: i)
             } else {
                 // 待匹配位置对应字符与pattern的最后一个字符不同
-                // 如果skipTable中存在待匹配位置对应字符，则后移对应的位置，否则后移整个pattern的长度
+                // 如果skipTable中存在待匹配位置对应字符，则后移对应的位置，对其lastChar与pattern中对应字符，否则后移整个pattern的长度
                 // 如果直到endIndex都没有，则表示匹配失败
                 i = index(i, offsetBy: skipTable[c] ?? patternLength, limitedBy: endIndex) ?? endIndex
             }
